a4254bfcf6d6b4fcbad3e008ecf5550b5dae5464
KantKontrol <nick.derissio.tech@gmail.com>
2020-03-01 19:53:50 -0500

    removed comments

--- app.js
+++ app.js
@@ -140,27 +140,12 @@ async function init(){
     //Call to Render HTML
     let renderedHtml = render(teamArray);
 
+
+    //output html to file
     writeHTML(OUTPUT_DIR, outputPath, renderedHtml);
 
-
-    //Build objects and pass them through to html renderer
     //clean up dry code
 }
 
-
-
-
-
 init();
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type
